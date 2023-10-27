2e92db3b3cfc777484a83c1e1054b9c61257f935
Juan <juanfelipecardenas4@gmail.com>
2023-10-27 19:31:17 -0400

    Added: knexconfig, Added: knex migrations (users table).

---
+++ server/src/routers/auth.router.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+//Auth router
+const router = express.Router();
+
+//Login
+router.post("/login", (req, res) => {});
+
+//Register
+router.post("/register", (req, res) => {});
+
+//Refresh token
+router.post("/refresh", (req, res) => {});
+
+export default router;
