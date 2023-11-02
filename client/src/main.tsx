7fe0aa8be14893e1164c3834e47e5b4ed0791233
Juan <juanfelipecardenas4@gmail.com>
2023-11-02 12:37:59 -0400

    Fixed: wave divider responsive design.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="theme--blue">
+    <div className="theme--green">
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
